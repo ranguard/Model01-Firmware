80999019306c2e0786de167148bdf0cdc08b5b8a
Leo Lapworth <leo@cuckoo.org>
2017-12-18 14:09:39 +0000

    switch tab to on space with func and return func to left

--- Model01-Firmware.ino
+++ Model01-Firmware.ino
@@ -140,7 +140,7 @@ const Key keymaps[][ROWS][COLS] PROGMEM = {
    Key_PageUp,   Key_A, Key_S, Key_D, Key_F, Key_G,
    Key_PageDown, Key_Z, Key_X, Key_C, Key_V, Key_B, Key_Escape,
    Key_LeftControl, Key_Backspace, Key_LeftGui, Key_LeftShift,
-   Key_Tab,
+   ShiftToLayer(FUNCTION),
 
    ___,  Key_6, Key_7, Key_8,     Key_9,         Key_0,    Key_RightAlt,
    ___,     Key_Y, Key_U, Key_I,     Key_O,         Key_P,         Key_Equals,
@@ -161,7 +161,7 @@ const Key keymaps[][ROWS][COLS] PROGMEM = {
    Consumer_PlaySlashPause,    Consumer_ScanNextTrack, Key_LeftCurlyBracket,     Key_RightCurlyBracket,    Key_LeftBracket, Key_RightBracket, Key_F12,
                                Key_LeftArrow,          Key_UpArrow,            Key_DownArrow,              Key_RightArrow,  ___,              ___,
    Key_PcApplication,          Key_Mute,               Consumer_VolumeDecrement, Consumer_VolumeIncrement, ___,             Key_Backslash,    Key_Pipe,
-   ___, ___, Key_Enter, ___,
+   ___, ___, Key_Tab, ___,
    ___),
 
 
